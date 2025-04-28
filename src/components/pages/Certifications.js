43e7460cf35a513d161eb0d4fa5b0a904deb101e
jnanaprakash <prakash143.thinkspace@gmail.com>
2025-04-28 13:06:50 +0530

    About, page and all pages hero banner added

--- src/components/pages/Certifications.js
+++ src/components/pages/Certifications.js
@@ -1,15 +1,36 @@
 import React from 'react'
+import { Helmet } from 'react-helmet';
+import Watsapp from '../Watsapp';
 
 export default function Certifications() {
   return (
-    <div>
-          <br></br>
-              <br></br>
-           
-              <br></br>  <br></br>
-              <br></br>
-              <br></br>
-      <h1>Page under development</h1>
+    <div className='certificates mt-80'>
+      <Helmet>
+        <title>Divine Bio Remedies | www.divinebioremedies.com</title>
+        <meta name='Baby care services' content='Baby care services' />
+
+      </Helmet>
+      <Watsapp />
+
+      <div className="hero-banner">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-7">
+              <h2 className="section-heading"><span className="blue"> Certificates </span><span className="yellow"></span></h2>
+              <h5>"Certificate Details."</h5>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1>Page under Development</h1>
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 }
